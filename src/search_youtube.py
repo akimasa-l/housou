31e5d10cc2df016f631478267c94b2f4613c8f02
akimasa-l <liumingzheng8@outlook.com>
2021-05-26 22:14:41 +0900

    youtubeで調べられるようになったぜ！

---
+++ src/search_youtube.py
@@ -0,0 +1,28 @@
+import json
+
+import requests
+
+with open("../youtube/api_key.txt") as f:
+    API_KEY = f.read().rstrip()
+#print("API key: " + API_KEY)
+
+
+def search(keyword: str):
+    url = f"https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&q={keyword}&key={API_KEY}"
+    response = requests.get(url)
+    response.encoding = "UTF-8"
+    data: dict = json.loads(response.text)
+    return data
+
+
+def get_url(data: dict):
+    return "https://www.youtube.com/watch?v="+data["items"][0]["id"]["videoId"]
+
+
+
+if __name__ == "__main__":
+    def main():
+        data=search("キセキ GReeeeN")
+        print(json.dumps(data, indent=4))
+        print(get_url(data))
+    main()
